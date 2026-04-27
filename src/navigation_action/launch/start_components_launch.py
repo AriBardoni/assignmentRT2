7683fd253415f0694ceb5c08f913a6474e777030
Ari <arianna.bardoni@gmail.com>
2026-04-27 13:03:05 +0200

    fixes but still obstacles

--- src/navigation_action/launch/start_components_launch.py
+++ src/navigation_action/launch/start_components_launch.py
@@ -3,26 +3,28 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
+
     container = ComposableNodeContainer(
         name='navigation_container',
+        namespace='',   # QUESTO È OBBLIGATORIO SU JAZZY
         package='rclcpp_components',
         executable='component_container_mt',
         output='screen',
+
         composable_node_descriptions=[
-            # Broadcaster TF2
+
             ComposableNode(
                 package='navigation_action',
                 plugin='navigation_action::RobotBroadcaster',
-                name='robot_broadcaster',
+                name='robot_broadcaster'
             ),
-            # Action Server
+
             ComposableNode(
                 package='navigation_action',
                 plugin='navigation_action::NavigationServer',
-                name='navigation_server',
-                extra_arguments=[{'use_intra_process_comms': True}],
+                name='navigation_server'
             ),
-            # NOTA: il client NON è più qui - va eseguito separatamente!
         ],
     )
+
     return launch.LaunchDescription([container])
